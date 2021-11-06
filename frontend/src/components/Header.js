f11db6c181671303b68aa9dcd15e66d874d7b357
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-11-06 14:05:31 +0100

    Add LinkContainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,3 +1,4 @@
+import { LinkContainer } from 'react-router-bootstrap';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
@@ -5,16 +6,26 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">E-shop</Navbar.Brand>
+          {/* <Navbar.Brand href="/">E-shop</Navbar.Brand> */}
+          <LinkContainer to="/">
+            <Navbar.Brand>E-shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
+              {/* <Nav.Link href="/cart">
                 <i className="fas fa-shopping-cart" /> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
+              </Nav.Link> */}
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart" /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
