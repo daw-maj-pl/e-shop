a5bc21f2a7047244d5b962a75c0ce5c7f3a5fd3a
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-12-14 19:38:46 +0100

    Update navbar name

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -171,11 +171,11 @@ export const updateUserProfile = user => async (dispatch, getState) => {
       type: USER_UPDATE_PROFILE_SUCCESS,
       payload: data
     });
-    // dispatch({
-    //   type: USER_LOGIN_SUCCESS,
-    //   payload: data
-    // });
-    // localStorage.setItem('userInfo', JSON.stringify(data));
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data
+    });
+    localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     const message =
       error.response && error.response.data.message
