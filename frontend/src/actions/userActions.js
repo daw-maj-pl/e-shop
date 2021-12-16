041809d0e291e246ed36c366905f188e7d947bd7
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-12-16 17:01:05 +0100

    Delete unneeded content type

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -126,7 +126,6 @@ export const getUserDetails = id => async (dispatch, getState) => {
 
     const config = {
       headers: {
-        'Content-Type': 'application/json',
         Authorization: `Bearer ${userInfo.token}`
       }
     };
